8a5efbdbe2fbe51c17db98355254d2130ade888f
João P. Bento <115121271+paulo-bento@users.noreply.github.com>
2026-03-06 21:36:29 -0300

    Add files via upload

---
+++ Exercicio003.java
@@ -0,0 +1,11 @@
+public class Exercicio003 {
+
+    public static void main(String[] args) {
+
+        int[] vetor = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]);
+        }
+    }
+}
